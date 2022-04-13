bcf0a4fb14b850cbd0e85a2150ff0d16f9acc9ec
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-04-13 17:48:54 +0530

    Added: email validation

--- src/com/bl/user_registation_regex/match/UserRegistration.java
+++ src/com/bl/user_registation_regex/match/UserRegistration.java
@@ -9,7 +9,7 @@ public class UserRegistration {
         UserDetails userDetails = new UserDetails();
         Scanner scanner = new Scanner(System.in);
         do {
-            System.out.println("1. First Name\n2. Last Name\n0. Exit");
+            System.out.println("1. First Name\n2. Last Name\n3. Email\n0. Exit");
             System.out.println("Enter choice");
             choice = scanner.nextInt();
             switch (choice) {
@@ -26,6 +26,13 @@ public class UserRegistration {
                     boolean isLastName = Pattern.matches("^[A-Z].[a-zA-Z0-9]{2}$", lastName);
                     if (isLastName) userDetails.setFirstName(lastName);
                     else System.out.println("Please check Last Name");
+                    break;
+                case 3:
+                    System.out.println("Enter Email");
+                    String email = scanner.next();
+                    boolean isMailId = Pattern.matches("^[a-z0-9]+[-/+.]*[0-9a-z]*@[a-z0-9]+.[a-z]{2,6}.[a-z]*$", email);
+                    if (isMailId) userDetails.setFirstName(email);
+                    else System.out.println("Please check Email Id Name");
                     break;
                 case 0:
                     break;
